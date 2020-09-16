9c2b593b7780876ad9e5d4d8983782cee9ccf2da
JVMarks <jovic.marks@gmail.com>
2020-09-16 15:17:27 -0300

    TypeForm

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Main, Section, Sticky } from './styles';
+
+import GlobalStyles from './styles/global';
+import Header from './components/Header';
+import FirstAndSecond from './components/FirstAndSecond';
+
+
+function App() {
+  React.useEffect(() => {
+    window.scrollTo({ top: 0 })
+  }, [])
+
+  return (
+    <>
+      <GlobalStyles />
+
+      <Main style={{ height: '1610vh' }}>
+        <Header />
+
+        <Section style={{ height: '72.7%' }}>
+          <FirstAndSecond />
+        </Section>
+
+        <Section style={{ height: '9.7%' }}>
+          <Sticky className="third" />
+        </Section>
+
+        <Section style={{ height: '10.1%' }}>
+          <Sticky className="fourth" />
+        </Section>
+
+
+      </Main>
+    </>
+  )
+}
+
+export default App
